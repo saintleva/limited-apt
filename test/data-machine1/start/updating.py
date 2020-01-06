6fefb75c83b0ac848cdc1c7c0b2e7b8f8e74ddf8
Anton Liaukevich <leva.dev@gmail.com>
2020-01-06 13:00:48 +0300

    Начал реализовать функционал, необходимый для подпоманд fix-interrupted и ignore-interrupted. (предыдущий комит был ошибочным)

---
+++ test/data-machine1/start/updating.py
@@ -0,0 +1,16 @@
+from datetime import *
+
+
+def is_distro_update_needed(last_update):
+    if last_update is None:
+        return True
+    today = date.today()
+    night_sync = datetime(today.year, today.month, today.day, 2)
+    return last_update < night_sync
+
+def is_enclosure_update_needed(last_update):
+    if last_update is None:
+        return True
+    today = date.today()
+    night_sync = datetime(today.year, today.month, today.day, 4)
+    return last_update < night_sync
